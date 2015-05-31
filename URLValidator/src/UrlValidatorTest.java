019fea547119a2e9cc3a2efbcc50bfab51e5b7d1
Kevin Parks <parkske@onid.oregonstate.edu>
2015-05-31 11:19:51 -0700

    moved initilization of manualURLs and schemas into test file so we don't need to carry around additional external resources

--- URLValidator/src/UrlValidatorTest.java
+++ URLValidator/src/UrlValidatorTest.java
@@ -16,7 +16,6 @@
  */
 
 
-import java.io.*;
 import java.util.ArrayList;
 import org.junit.Rule;
 import org.junit.Test;
@@ -30,7 +29,9 @@ import static org.hamcrest.CoreMatchers.equalTo;
  * @version $Revision: 1128446 $ $Date: 2011-05-27 13:29:27 -0700 (Fri, 27 May 2011) $
  */
 public class UrlValidatorTest {
-
+   private ArrayList<ResultPair> m_schemas = this.InitializeSchemas();
+   private ArrayList<ResultPair> m_fileUrls = this.InitializeManualUrls();
+	
    @Rule
    public ErrorCollector collector = new ErrorCollector();
    
@@ -44,9 +45,8 @@ public class UrlValidatorTest {
 	   //sanity check
 	   ResultPair sane = new ResultPair("http://www.amazon.com", true);
 	   collector.checkThat(sane.item, sane.valid, equalTo(urlVal.isValid(sane.item)));
-	   //test from files by creating ResultPairs, then looping on them
-	   ArrayList<ResultPair> fileUrls = this.createPairsFromFile("src/UrlsToVerify.txt");
-	   for(ResultPair res : fileUrls)
+	   //test manualUrls
+	   for(ResultPair res : this.m_fileUrls)
 	   {
 		   collector.checkThat(res.item, res.valid, equalTo(urlVal.isValid(res.item)));
 	   }
@@ -54,20 +54,21 @@ public class UrlValidatorTest {
    
    /**
     *  Creates a set of URI schemes to test with
-    *  list from: http://www.iana.org/assignments/uri-schemes/uri-schemes.xhtml
+    *  
     */
    @Test
    public void testYourFirstPartition()
    {
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	   ArrayList<String> schemas = this.CreateStringList("src/Schemas.txt");
 	   String resource = "://www.example.com";
 	   ArrayList<ResultPair> schemaUrls = new ArrayList<ResultPair>();
-	   for(String schema : schemas)
+	   //append the static valid resource string to each schema as a new pair
+	   for(ResultPair schema : this.m_schemas)
 	   {
-		   ResultPair pair = new ResultPair(schema + resource, true);
+		   ResultPair pair = new ResultPair(schema.item + resource, schema.valid);
 		   schemaUrls.add(pair);
 	   }
+	   //test each now full URL
 	   for(ResultPair pair : schemaUrls)
 	   {
 		   collector.checkThat(pair.valid, equalTo(urlVal.isValid(pair.item)));
@@ -99,65 +100,131 @@ public class UrlValidatorTest {
     *
     * @param testObjects Used to create a url.
     */
+
    
    /**
-    * Creates a list of ResultPairs from a well-formed text file
-    * @param filepath
+    * Initializes manual URLs for testing, not expected to be used elsewhere
     * @return
     */
-   private ArrayList<ResultPair> createPairsFromFile(String filepath)
+   private ArrayList<ResultPair> InitializeManualUrls()
    {
-	   ArrayList<ResultPair> results = new ArrayList<ResultPair>();
-	   try {
-			File file = new File(filepath);
-			FileReader fileReader = new FileReader(file);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
-				String[] parts = line.split("\t");
-				if(parts.length < 2)
-				{
-					String[] tmp = {"", parts[0]};
-					parts = tmp;
-				}
-				boolean expect;
-				if(parts[1].contentEquals("invalid"))
-					expect = false;
-				else if(parts[1].contentEquals("valid"))
-					expect = true;
-				else
-					continue;
-				ResultPair result = new ResultPair(parts[0], expect);
-				result.item = parts[0];
-				results.add(result);
-			}
-			fileReader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	return results;
+	   ArrayList<ResultPair> fileUrls = new ArrayList<ResultPair>();
+	   fileUrls.add(new ResultPair("https://www.google.com", true));
+	   fileUrls.add(new ResultPair("https://///www.google.com", false));
+	   fileUrls.add(new ResultPair("", false));
+	   fileUrls.add(new ResultPair("http://74.125.224.72/", true));
+	   fileUrls.add(new ResultPair("file:///C:/", true));
+	   fileUrls.add(new ResultPair("http://WWW.GOOGLE.COM", true));
+	   fileUrls.add(new ResultPair("http://www.google.com:80/test1", true));
+	   fileUrls.add(new ResultPair("h3t://go.cc:65a/$23?action=edit&mode=up", false));
+	   fileUrls.add(new ResultPair("12345", false));
+	   fileUrls.add(new ResultPair("http://www.space in here.com", false));
+	   fileUrls.add(new ResultPair("http://site.com/#citation", true));
+	   fileUrls.add(new ResultPair("ftp://site.com", true));
+	   fileUrls.add(new ResultPair("http://site.com/hyphen-here", true));
+	   fileUrls.add(new ResultPair("foo://example.com:8042/over/there?name=ferret#nose", true));
+	   fileUrls.add(new ResultPair("http://user@example.com", true));
+	   fileUrls.add(new ResultPair("http://142.10.5.2:8080/", true));
+	   return fileUrls;
    }
    
    /**
-    * Grabs a line separated list of data from a file
-    * @param filepath
+    * Initializes a list of schemas into ResultPairs
+    * list from: http://www.iana.org/assignments/uri-schemes/uri-schemes.xhtml
     * @return
     */
-   private ArrayList<String> CreateStringList(String filepath)
+   private ArrayList<ResultPair> InitializeSchemas()
    {
-	   ArrayList<String> results = new ArrayList<String>();
-	   try {
-			File file = new File(filepath);
-			FileReader fileReader = new FileReader(file);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
-				results.add(line);
-			}
-			fileReader.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	   return results;
+	   ArrayList<ResultPair> schemas = new ArrayList<ResultPair>();
+	   schemas.add(new ResultPair("z39.50s", true));
+	   schemas.add(new ResultPair("z39.50r", true));
+	   schemas.add(new ResultPair("xmpp", true));
+	   schemas.add(new ResultPair("xmlrpc.beeps", true));
+	   schemas.add(new ResultPair("xmlrpc.beep", true));
+	   schemas.add(new ResultPair("xcon-userid", true));
+	   schemas.add(new ResultPair("xcon", true));
+	   schemas.add(new ResultPair("wss", true));
+	   schemas.add(new ResultPair("ws", true));
+	   schemas.add(new ResultPair("vemmi", true));
+	   schemas.add(new ResultPair("urn", true));
+	   schemas.add(new ResultPair("tv", true));
+	   schemas.add(new ResultPair("turns", true));
+	   schemas.add(new ResultPair("turn", true));
+	   schemas.add(new ResultPair("tn3270", true));
+	   schemas.add(new ResultPair("tip", true));
+	   schemas.add(new ResultPair("thismessage", true));
+	   schemas.add(new ResultPair("tftp", true));
+	   schemas.add(new ResultPair("telnet", true));
+	   schemas.add(new ResultPair("tel", true));
+	   schemas.add(new ResultPair("tag", true));
+	   schemas.add(new ResultPair("stuns", true));
+	   schemas.add(new ResultPair("stun", true));
+	   schemas.add(new ResultPair("soap.beeps", true));
+	   schemas.add(new ResultPair("soap.beep", true));
+	   schemas.add(new ResultPair("snmp", true));
+	   schemas.add(new ResultPair("sms", true));
+	   schemas.add(new ResultPair("sips", true));
+	   schemas.add(new ResultPair("sip", true));
+	   schemas.add(new ResultPair("sieve", true));
+	   schemas.add(new ResultPair("shttp", true));
+	   schemas.add(new ResultPair("session", true));
+	   schemas.add(new ResultPair("service", true));
+	   schemas.add(new ResultPair("rtspu", true));
+	   schemas.add(new ResultPair("rtsps", true));
+	   schemas.add(new ResultPair("rtsp", true));
+	   schemas.add(new ResultPair("reload", true));
+	   schemas.add(new ResultPair("pres", true));
+	   schemas.add(new ResultPair("pop", true));
+	   schemas.add(new ResultPair("pkcs11", true));
+	   schemas.add(new ResultPair("opaquelocktoken", true));
+	   schemas.add(new ResultPair("nntp", true));
+	   schemas.add(new ResultPair("nih", true));
+	   schemas.add(new ResultPair("ni", true));
+	   schemas.add(new ResultPair("nfs", true));
+	   schemas.add(new ResultPair("news", true));
+	   schemas.add(new ResultPair("mupdate", true));
+	   schemas.add(new ResultPair("mtqp", true));
+	   schemas.add(new ResultPair("msrps", true));
+	   schemas.add(new ResultPair("msrp", true));
+	   schemas.add(new ResultPair("mid", true));
+	   schemas.add(new ResultPair("mailto", true));
+	   schemas.add(new ResultPair("ldap", true));
+	   schemas.add(new ResultPair("jabber", true));
+	   schemas.add(new ResultPair("iris.xpcs", true));
+	   schemas.add(new ResultPair("iris.xpc", true));
+	   schemas.add(new ResultPair("iris.lwz", true));
+	   schemas.add(new ResultPair("iris.beep", true));
+	   schemas.add(new ResultPair("iris", true));
+	   schemas.add(new ResultPair("ipps", true));
+	   schemas.add(new ResultPair("ipp", true));
+	   schemas.add(new ResultPair("info", true));
+	   schemas.add(new ResultPair("imap", true));
+	   schemas.add(new ResultPair("im", true));
+	   schemas.add(new ResultPair("icap", true));
+	   schemas.add(new ResultPair("iax", true));
+	   schemas.add(new ResultPair("https", true));
+	   schemas.add(new ResultPair("http", true));
+	   schemas.add(new ResultPair("h323", true));
+	   schemas.add(new ResultPair("gopher", true));
+	   schemas.add(new ResultPair("go", true));
+	   schemas.add(new ResultPair("geo", true));
+	   schemas.add(new ResultPair("ftp", true));
+	   schemas.add(new ResultPair("file", true));
+	   schemas.add(new ResultPair("example", true));
+	   schemas.add(new ResultPair("dns", true));
+	   schemas.add(new ResultPair("dict", true));
+	   schemas.add(new ResultPair("dav", true));
+	   schemas.add(new ResultPair("data", true));
+	   schemas.add(new ResultPair("crid", true));
+	   schemas.add(new ResultPair("coaps", true));
+	   schemas.add(new ResultPair("coap", true));
+	   schemas.add(new ResultPair("cid", true));
+	   schemas.add(new ResultPair("cap", true));
+	   schemas.add(new ResultPair("acct", true));
+	   schemas.add(new ResultPair("acap", true));
+	   schemas.add(new ResultPair("about", true));
+	   schemas.add(new ResultPair("aaas", true));
+	   schemas.add(new ResultPair("aaa", true));
+	   return schemas;
    }
 }
