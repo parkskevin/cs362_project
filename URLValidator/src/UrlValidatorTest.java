0b7cd6da6542391d8aaf34b8c4a2759271878f89
khangd <duongk@onid.oregonstate.edu>
2015-06-05 08:56:50 -0700

    added second partition

--- URLValidator/src/UrlValidatorTest.java
+++ URLValidator/src/UrlValidatorTest.java
@@ -77,7 +77,37 @@ public class UrlValidatorTest {
    
    @Test
    public void testYourSecondPartition(){
-	   
+
+		boolean result;
+
+		UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+
+		System.out.println("\n########  SECOND INPUT PARTITION TEST BEGIN #########\n");
+
+		String testURL = "http://www.google.";
+
+		//Country code array to test all possible valid country codes with Googles URl.
+		String cuntryCodez[] = {"A1","A2","O1","AD","AE","AF","AG","AI","AL","AM","AO","AP",
+		"AQ","AR","AS","AT","AU","AW","AX","AZ","BA","BB","BD","BE","BF","BG","BH",
+		"BI","BJ","BL","BM","BN","BO","BQ","BR","BS","BT","BV","BW","BY","BZ","CA",
+		"CC","CD","CF","CG","CH","CI","CK","CL","CM","CN","CO","CR","CU","CV","CW",
+		"CX","CY","CZ","DE","DJ","DK","DM","DO","DZ","EC","EE","EG","EH","ER","ES",
+		"ET","EU","FI","FJ","FK","FM","FO","FR","GA","GB","GD","GE","GF","GG","GH",
+		"GI","GL","GM","GN","GP","GQ","GR","GS","GT","GU","GW","GY","HK","HM","HN",
+		"HR","HT","HU","ID","IE","IL","IM","IN","IO","IQ","IR","IS","IT","JE","JM",
+		"JO","JP","KE","KG","KH","KI","KM","KN","KP","KR","KW","KY","KZ","LA","LB",
+		"LC","LI","LK","LR","LS","LT","LU","LV","LY","MA","MC","MD","ME","MF","MG",
+		"MH","MK","ML","MM","MN","MO","MP","MQ","MR","MS","MT","MU","MV","MW","MX",
+		"MY","MZ","NA","NC","NE","NF","NG","NI","NL","NO","NP","NR","NU","NZ","OM",
+		"PA","PE","PF","PG","PH","PK","PL","PM","PN","PR","PS","PT","PW","PY","QA",
+		"RE","RO","RS","RU","RW","SA","SB","SC","SD","SE","SG","SH","SI","SJ","SK",
+		"SL","SM","SN","SO","SR","SS","ST","SV","SX","SY","SZ","TC","TD","TF","TG",
+		"TH","TJ","TK","TL","TM","TN","TO","TR","TT","TV","TW","TZ","UA","UG","UM",
+		"US","UY","UZ","VA","VC","VE","VG","VI","VN","VU","WF","WS","YE","YT","ZA","ZM","ZW"};
+
+		for(int i = 0 ; i < cuntryCodez.length ; i++){
+			result = urlVal.isValid(testURL + cuntryCodez[i]); //Loops and appends a new country code to google for each test.
+			System.out.println(testURL + cuntryCodez[i] + " : " + result + " : expected result: True.");	   
    }
    
    @Test
